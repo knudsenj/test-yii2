77a7804e43e619145a4a56fec1553d6a9d3c2606
Joel Knudsen <joel@anglersatlas.com>
2015-04-18 14:00:52 -0700

    add in the backend user model

---
+++ backend/modules/v1/models/User.php
@@ -0,0 +1,52 @@
+<?php
+namespace common\models;
+
+use Yii;
+use yii\base\NotSupportedException;
+use yii\behaviors\TimestampBehavior;
+use yii\db\ActiveRecord;
+use yii\db\Expression;
+use yii\web\IdentityInterface;
+
+/**
+ * User model
+ *
+ * @property integer $id
+ * @property string $username
+ * @property string $password_hash
+ * @property string $password_reset_token
+ * @property string $email
+ * @property string $auth_key
+ * @property integer $status
+ * @property integer $created_at
+ * @property integer $updated_at
+ * @property string $password write-only password
+ */
+class User extends \common\models\User
+{
+    public function scenarios(){
+        return [
+            'create' => ['username', 'email', 'password'],
+            'update' => ['email', 'password'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        $rules = parent::rules();
+
+        $rules[] = [['username', 'email', 'password'], 'required', 'on'=>'create'];
+        $rules[] = ['email', 'email'];
+        $rules[] = ['username', 'match', 'pattern' => '/^\w*$/i'];
+        $rules[] = ['password', 'string', 'min' => 5];
+        $rules[] = ['email', 'unique', 'lower(email)'];
+        $rules[] = ['username', 'unique', 'lower(username)'];
+    }
+
+    public function getPassword(){
+        return $this->password_hash;
+    }
+}
